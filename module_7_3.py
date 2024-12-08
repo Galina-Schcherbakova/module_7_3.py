65973ef9ec3271dbf26bd2dad7d7e29cbf42e085
Galina-Schcherbakova <galchonisch@mail.ru>
2024-12-08 12:18:38 +0300

    module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,26 +1,39 @@
-class WordsFinder:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.words = self._load_words()
+import string
 
-    def _load_words(self):
-        with open(self.file_path, 'r', encoding='utf-8') as file:
-            text = file.read()
-            return text.lower().split()
+class WordsFinder:
+    def __init__(self, *file_paths):
+        self.file_paths = file_paths
+        self.all_words = {file_path: self._load_words(file_path) for file_path in self.file_paths}
+
+    def _load_words(self, file_path):
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                text = file.read()
+                text = text.translate(str.maketrans('', '', string.punctuation))
+                return text.lower().split()
+        except FileNotFoundError:
+            print(f"Файл {file_path} не найден.")
+            return []
 
     def get_all_words(self):
-        return self.words
+        return self.all_words
 
     def find(self, word):
         word_lower = word.lower()
-        try:
-            return self.words.index(word_lower) + 1
-        except ValueError:
-            return -1
+        results = {}
+        for file_path, words in self.all_words.items():
+            try:
+                results[file_path] = words.index(word_lower) + 1
+            except ValueError:
+                results[file_path] = -1
+        return results
 
     def count(self, word):
         word_lower = word.lower()
-        return self.words.count(word_lower)
+        results = {}
+        for file_path, words in self.all_words.items():
+            results[file_path] = words.count(word_lower)
+        return results
 
 
 if __name__ == "__main__":
